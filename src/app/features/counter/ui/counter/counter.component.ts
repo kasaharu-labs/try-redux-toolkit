5f5af833b99ddc1ce352be87e69325b7087a236c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-21 10:16:45 +0900

    counter の状態を view に反映

--- src/app/features/counter/ui/counter/counter.component.ts
+++ src/app/features/counter/ui/counter/counter.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { actions, createSelector, store } from '../../store';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  counter = createSelector<number>((state) => state.counter);
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onClick() {
+    store.dispatch(actions.increment());
   }
-
 }
